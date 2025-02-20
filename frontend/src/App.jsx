e2428c4fa3e2587d0776ad01caee7282f3c0157e
NGiudiDev <ngiudice.dev@gmail.com>
2025-02-19 23:11:57 -0300

    fix linter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import { useAutomaticLogin } from "./modules/session/hooks/useAutomaticLogin";
 import { useAppRoutes } from "./modules/common/hooks";
 
-import { CustomRouteLayout, NotFoundMessageLayout, PageLoadingLayout } from './modules/common/components';
+import { CustomRouteLayout, NotFoundMessageLayout, PageLoadingLayout } from "./modules/common/components";
 
 export const App = () => {
 	const automaticLogin = useAutomaticLogin();
